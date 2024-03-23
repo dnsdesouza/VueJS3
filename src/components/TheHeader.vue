af4d9caacb7deb41d728e96785535b90d1feb687
dnsdesouza <dnsdesouza1@gmail.com>
2024-03-22 22:29:41 -0300

    VD Aula 05

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,18 +1,70 @@
 <template>
-    <header class="header">
-        Header
-    </header>
+
+    <div 
+        v-for="(obj, index) in todos"
+        v-bind:key="obj.id"
+        class="todos-item"
+    >
+     {{ index }}   {{ obj.title }}
+    </div>
 
 </template>
 
 <script>
-    export default{
+export default {    
+    name: 'App',
+    data() {
+        return {
+            todos: [
+                {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "delectus aut autem",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 2,
+                    "title": "quis ut nam facilis et officia qui",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 3,
+                    "title": "fugiat veniam minus",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 4,
+                    "title": "et porro tempora",
+                    "completed": true
+                },
+                {
+                    "userId": 1,
+                    "id": 5,
+                    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                    "completed": false
+                }
+            ]
 
+        }
     }
+}
 </script>
 <style>
-    .header{
-        background: #000;
-        color: #fff;
-    }
+.todos-item {
+    background: #000;
+    margin: 0 0 5px 0;
+    padding: 3px 6px;
+    color: #fff
+}
+#app {
+    font-family: Avenir, Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-aling: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
 </style>
