fe75a16b6a6ce220ca889c5cdd465d426b471046
dnsdesouza <dnsdesouza1@gmail.com>
2024-03-24 01:24:12 -0300

    VueJs #07

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <primeiro-componente />
+    <p>
+        O dado de app é {{ testando }}
+    </p>
+</template>
+
+<script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+
+export default {
+    name: 'App',
+    components: {
+        PrimeiroComponente
+    },
+    data() {
+        return {
+            testando: "Teste"
+        }
+    }
+}
+</script>
